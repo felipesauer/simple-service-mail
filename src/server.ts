e1de95d4dfe61ad7ae566a3991e54e7bdbb996bb
felipesauer <felipesauer11@hotmail.com>
2023-02-26 16:38:44 -0400

    - Estrutura de projeto

---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import App from "@shared/app";
+
+new App();
---
+++ src/shared/app.ts
@@ -0,0 +1,9 @@
+import "@shared/infra/messages/rabbitmq/Rabbitmq";
+
+class App {
+    constructor() {
+        console.log("Application restarted successfully");
+    }
+}
+
+export default App;
