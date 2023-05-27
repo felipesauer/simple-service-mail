7c9420a4a87c0dae8583a68ad0bdba433a4d41f6
felipesauer <felipesauer11@hotmail.com>
2023-05-27 18:59:52 -0300

    - Criado teste para main.send

--- src/modules/mail/infra/http/consumers/mail.send.spec.ts
+++ src/modules/mail/infra/http/consumers/mail.send.spec.ts
@@ -1,20 +1,31 @@
 import MailSend from "./mail.send";
 import MailController from "../controllers/MailController";
 
+jest.mock("@shared/infra/messages/rabbitmq/Rabbitmq", () => jest.fn());
+
 let mailSend: MailSend;
+let messageMock: string;
 
 describe("mail.send", () => {
     beforeAll(() => {
-        const sendMailMock = jest
-            .spyOn(MailController.prototype, "sendMail")
-            .mockImplementation(jest.fn());
+        jest.spyOn(MailController.prototype, "sendMail").mockResolvedValue();
 
-        const callbackPublishQueueMock = jest
-            .spyOn(MailSend.prototype, "callbackPublishQueue")
-            .mockImplementation(jest.fn());
+        jest.spyOn(
+            MailSend.prototype,
+            "callbackPublishQueue",
+        ).mockResolvedValue();
+
+        messageMock =
+            '{"server":{"host":"smtp.gmail.com","port":465,"secure":1,"auth":{"user":"user@user.com","pass":"user123"}},"content":{"from":"Test Mail 👻","to":"test@test.com","subject":"Test","text":"Test Mail"}}';
 
         mailSend = new MailSend();
     });
 
-    it("should be able execute mail", async () => {});
+    it("should be able execute mail", () => {
+        expect(mailSend.execute(messageMock)).resolves;
+    });
+
+    it("should not be able execute mail", () => {
+        expect(mailSend.execute("")).rejects;
+    });
 });
